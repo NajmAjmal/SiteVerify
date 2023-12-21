1107bcf49b15c58be6f44bbe515da560df99c1f7
Najm Ajmal <151127614+NajmAjmal@users.noreply.github.com>
2023-12-21 18:20:34 +0000

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,15 +1,22 @@
+// Clear console and add blank line
+console.clear();
+console.log('\n');
+
 // Retrieve the meta tag content
 const metaTag = document.querySelector('meta[name="site-verification"]');
 const metaString = metaTag ? metaTag.getAttribute('content') : '';
 
-// Log a warning to the console
-console.group('%cSite Verification Warning', 'color: #FF5733; font-size: 18px; font-weight: bold;');
+// Increase the size of the warning
+console.group('%cSite Verification Warning', 'color: #FF5733; font-size: 20px; font-weight: bold;');
 console.warn('Make sure this site is real. Verify at:');
-console.log(`%chttps://raw.githubusercontent.com/NajmAjmal/SiteVerify/main/sites/${metaString}`, 'color: #3366FF; text-decoration: underline;');
+console.log(`%c(PGP SIGN) https://raw.githubusercontent.com/NajmAjmal/SiteVerify/main/sites/${metaString}`, 'color: #3366FF; text-decoration: underline;');
 console.groupEnd();
 
 // Instructions for PGP verification
 console.group('%cPGP Verification Instructions', 'color: #33CC33; font-size: 16px; font-weight: bold;');
 console.log('To verify PGP, use Najm Ajmal\'s public key:');
-console.log('%chttps://github.com/NajmAjmal/NajmAjmal/PGP', 'color: #3366FF; text-decoration: underline;');
+console.log('%c(PGP PUB)  https://raw.githubusercontent.com/NajmAjmal/NajmAjmal/main/PGP', 'color: #3366FF; text-decoration: underline;');
+console.log('Use this website to verify the signature:');
+console.log('%chttps://pgp.najm.uk', 'color: #3366FF; text-decoration: underline;');
 console.groupEnd();
+console.log('\n');
